8533c11d13416409dfdc4baeb702c3d072711e33
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-09-08 23:54:57 +0500

    React Componenet

---
+++ React-JS/Class-03/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Componenets/navbar";
+import Body from "./Componenets/body";
+import Footer from "./Componenets/footer";
+const App = () => {
+  return (
+  <div>
+      <Navbar />
+    <Body />
+    <Footer />
+  </div>
+  )
+}
+
+export default App
---
+++ React-JS/Class-03/src/Componenets/body.jsx
@@ -0,0 +1,8 @@
+import "../style.css"
+import React from "react"
+const Body = ()=>{
+    return (
+        <h1 class="heading">Body</h1>
+    )
+}
+export default Body
