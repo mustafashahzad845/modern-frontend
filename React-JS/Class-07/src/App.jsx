effec833189db825a73135114d014ceb30698652
mustafashahzad845 <mustafashahzad845@gmail.com>
2026-09-21 01:47:58 +0500

    React Input field control

--- React-JS/Class-07/src/App.jsx
+++ React-JS/Class-07/src/App.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
 const App = () => {
   const [arr , setArr] = useState(["Karachi" , "Lahore" , "Hyderabad"])
-  // const uiArray = 
+  // const uiArray = {
+//   arr.map((city , index)=>{
+//     return (
+//       <h1 key={index}> {city} {index + 1} </h1>
+//     )
+//   })
+// } 
+ const getInputVal = ()=>{
+    console.log(inputVal);
+    
+  }
+  const setInputContVal = ()=>{
+    setInputVal("Mustafa")
+  }
+  const [inputVal , setInputVal] = useState("")
+   
+  console.log(inputVal);
+  
   return (
     <div>
-{
+{/* {
   arr.map((city , index)=>{
     return (
       <h1 key={index}> {city} {index + 1} </h1>
     )
   })
-}
+} */}
+<input id="input" type="text" placeholder="Enter Your Name" onChange={(e)=>setInputVal(e.target.value) 
+} value={inputVal}/>
+<button onClick={getInputVal}>Get Value</button>
+<button onClick={setInputContVal}>Set Value</button>
+<p>{inputVal}</p>
+
     </div>
   )
 }
